143f9bdfa0238eb63fd10ba7ec981b60a835b029
“mayuri” <mayurisalunkhe01@gmail.com>
2021-11-21 15:03:53 +0530

    Adding AddressBook Database to remote

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -13,7 +13,13 @@ select * from AddressBookTable;
 SELECT * FROM AddressBookTable;
 INSERT INTO AddressBookTable (FirstName,LastName,Address,City,State,Zip,PhoneNumber,email) VALUES ('Mayuri', 'Salunkhe', 'Shahupuri','Satara', 'Maharashtra', 415002, 9960042360,'mayuri.salunkhe01@gmail.com');
 INSERT INTO AddressBookTable (FirstName,LastName,Address,City,State,Zip,PhoneNumber,email) VALUES ('Pratiksha', 'Jadhav', 'SamarthSadan','Pune', 'Maharashtra', 415001, 9213852136,'pratiksha.jadhav01@gmail.com');
+INSERT INTO AddressBookTable (FirstName,LastName,Address,City,State,Zip,PhoneNumber,email) VALUES ('Aditi', 'Bhosale', 'Shahupuri','Satara', 'Bangalore', 415003, 9960784236,'aditi.bhosale@gmail.com');
 
 ----------UC4-Edit Existing Contact Using Name-----
 UPDATE AddressBookTable SET FirstName='Pooja' ,LastName='Dere', email='poojadere@gmail.com' where FirstName='Mayuri';
 SELECT * FROM AddressBookTable;
+
+-----------UC5-Delete person using first name------------
+DELETE FROM AddressBookTable WHERE FirstName='Pooja';
+
+
