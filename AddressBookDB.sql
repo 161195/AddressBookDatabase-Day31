24b3f8babc4b3b21a7a9af98b408e74fc4fe4b66
“mayuri” <mayurisalunkhe01@gmail.com>
2021-11-21 14:37:53 +0530

    Adding AddressBook Database to remote

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -3,3 +3,8 @@
 -----------UC1-Create Database----------
 CREATE DATABASE AddressBookService;
 USE  AddressBookService;
+
+---------UC2- Create Table---------..
+CREATE TABLE AddressBookTable (FirstName varchar(25), LastName varchar(25), Address varchar(30) ,
+		City varchar(20), State varchar(30), Zip int, PhoneNumber bigint , email varchar(30) );
+select * from AddressBookTable;
